b78e4a5ef2bb5f8fbae242ae4467a10315ff11c7
Danii47 <uranux1@gmail.com>
2026-01-03 14:56:14 +0100

    feat: add more colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -6,4 +6,22 @@ export const ACCOUNT_COLORS = [
   "#8e44ad", // Purple
   "#34495e", // Dark Gray
   "#e91e63", // Pink
+  "#1abc9c", // Turquoise
+  "#d35400", // Dark Orange
+  "#7f8c8d", // Gray
+  "#009688", // Teal
+  "#3F51B5", // Indigo
+  "#673AB7", // Deep Purple
+  "#FF5722", // Deep Orange
+  "#795548", // Brown
+  "#607D8B", // Blue Grey
+  "#00BCD4", // Cyan
+  "#8BC34A", // Light Green
+  "#FFC107", // Amber
+  "#9C27B0", // Magenta
+  "#000000", // Black
+  "#4E342E", // Dark Brown
+  "#0D47A1", // Navy Blue
+  "#1B5E20", // Forest Green
+  "#B71C1C", // Blood Red
 ];
