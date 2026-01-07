1771281a6b036765ac9a108173307fffaaf1a583
Danii47 <uranux1@gmail.com>
2026-01-07 02:48:05 +0100

    style: add colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,27 +1,37 @@
 export const ACCOUNT_COLORS = [
-  "#2e78b7", // Blue
   "#e74c3c", // Red
-  "#27ae60", // Green
-  "#f39c12", // Orange
-  "#8e44ad", // Purple
-  "#34495e", // Dark Gray
-  "#e91e63", // Pink
-  "#1abc9c", // Turquoise
-  "#d35400", // Dark Orange
-  "#7f8c8d", // Gray
-  "#009688", // Teal
-  "#3F51B5", // Indigo
-  "#673AB7", // Deep Purple
-  "#FF5722", // Deep Orange
-  "#795548", // Brown
-  "#607D8B", // Blue Grey
-  "#00BCD4", // Cyan
-  "#8BC34A", // Light Green
-  "#FFC107", // Amber
-  "#9C27B0", // Magenta
-  "#000000", // Black
-  "#4E342E", // Dark Brown
-  "#0D47A1", // Navy Blue
-  "#1B5E20", // Forest Green
   "#B71C1C", // Blood Red
+  "#e91e63", // Pink
+  "#C2185B", // Rose Red
+
+  "#FF5722", // Deep Orange
+  "#d35400", // Dark Orange
+  "#f39c12", // Orange
+  "#FFC107", // Amber
+  "#FBC02D", // Dark Yellow
+
+  "#8BC34A", // Light Green
+  "#27ae60", // Green
+  "#1B5E20", // Forest Green
+  "#009688", // Teal
+  "#1abc9c", // Turquoise
+
+  "#00BCD4", // Cyan
+  "#2e78b7", // Blue
+  "#1976D2", // Royal Blue
+  "#0D47A1", // Navy Blue
+  "#3F51B5", // Indigo
+  "#607D8B", // Blue Grey
+
+  "#673AB7", // Deep Purple
+  "#8e44ad", // Purple
+  "#9C27B0", // Magenta
+
+  "#795548", // Brown
+  "#4E342E", // Dark Brown
+
+  "#95a5a6", // Light Gray
+  "#7f8c8d", // Gray
+  "#34495e", // Dark Gray
+  "#000000", // Black
 ];
