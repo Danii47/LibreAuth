52af78034a06a1cf313a621a046ea81ebbaac377
Danii47 <uranux1@gmail.com>
2026-01-07 02:23:27 +0100

    feat: create constant information of app

---
+++ constants/AppInformation.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = "LibreAuth";
+export const APP_VERSION = "v1.0.0";
+
+export const APP_PRIVACY_POLICY_URL = "https://github.com/Danii47/LibreAuth/blob/main/PRIVACY.md";
+export const APP_GITHUB_URL = "https://github.com/Danii47/LibreAuth";
+
+export const APP_DEVELOPER = "Danii47";
+export const APP_DEVELOPER_URL = "https://github.com/Danii47/";
