3a61e9439fc3b8dbaf72994f0142f23f4aab0b1e
Danii47 <uranux1@gmail.com>
2026-01-19 04:05:00 +0100

    feat: update app version to v1.0.6 in AppInformation

--- constants/AppInformation.ts
+++ constants/AppInformation.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "LibreAuth";
-export const APP_VERSION = "v1.0.0";
+export const APP_VERSION = "v1.0.6";
 
 export const APP_PRIVACY_POLICY_URL = "https://github.com/Danii47/LibreAuth/blob/main/PRIVACY.md";
 export const APP_GITHUB_URL = "https://github.com/Danii47/LibreAuth";
